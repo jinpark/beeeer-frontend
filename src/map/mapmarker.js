ec327be17b7a69010743cc714806d90e1ad4b6a5
Jin Park <jin+github@jinpark.net>
2017-02-12 21:33:55 +0900

    modal and tooltip

--- src/map/mapmarker.js
+++ src/map/mapmarker.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import Tooltip from './tooltip'
 
 import './mapmarker.css';
@@ -11,16 +12,18 @@ class MapMarker extends Component {
   constructor(props) {
      super(props);
      this.state = {open: false};
-     this.openTooltip = this.openTooltip.bind(this);
+     this.openTooltipAndCenter = this.openTooltipAndCenter.bind(this);
    }
 
-  openTooltip() {
+  openTooltipAndCenter() {
     this.setState({open: !this.state.open});
+    this.props.updateLocation(this.props.place.lat, this.props.place.lon);
   }
+
 
   render() {
     return (
-      <div className="mapmarker" onClick={this.openTooltip}>  
+      <div className="mapmarker" onClick={this.openTooltipAndCenter}>  
         <Tooltip $hover={this.props.$hover} place={this.props.place} open={this.state.open} />
         <i className="fa fa-beer fa-3x" aria-hidden="true"></i>
       </div>
@@ -28,4 +31,23 @@ class MapMarker extends Component {
   }
 }
 
-export default MapMarker;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateLocation: (latitude, longitude) => {
+      dispatch({type: 'SET_LATLNG', lat: latitude, lng: longitude})
+    },
+    openTooltip: (key) => {
+      dispatch({type: 'OPEN_TOOLTIP', key: key})
+    },
+    closeTooltip: (key) => {
+      dispatch({type: 'CLOSE_TOOLTIP'})
+    }
+  }
+}
+
+const MapMarkerPage = connect(
+  null,
+  mapDispatchToProps
+)(MapMarker)
+
+export default MapMarkerPage;
--- src/map/tooltip.js
+++ src/map/tooltip.js
@@ -3,7 +3,21 @@ import ReactModal from 'react-modal';
 
 import './tooltip.css';
 
-const customModalStyle = {content: {top: "100px"}};
+const customModalStyle = {content: {border: '0',
+                                    borderRadius: '4px',
+                                    bottom: 'auto',
+                                    minHeight: '10rem',
+                                    maxHeight: 'calc(80% - 100px)',
+                                    left: '50%',
+                                    padding: '2rem',
+                                    position: 'fixed',
+                                    right: 'auto',
+                                    top: 'calc(50% + 50px)',
+                                    transform: 'translate(-50%,-50%)',
+                                    minWidth: '20rem',
+                                    width: '80%',
+                                    maxWidth: '60rem'
+                                  }};
 
 class Tooltip extends Component {
 
@@ -30,17 +44,28 @@ class Tooltip extends Component {
       opacity: this.props.open ? 1 : 0
     };
 
+
     return (
       <div className="tooltip-modal" style={hover} >
         <div className="tooltip">  
-          <h1>{this.props.place.name}</h1>
-          <hr />
-          <div>{this.props.place.description}</div>
-          <button onClick={this.handleOpenModal}>Click</button>
+          <div className="tooltip__title">
+          <h1>{this.props.place.name}</h1> 
+          <button onClick={this.handleOpenModal}>
+            <i className="fa fa-chevron-right fa-2x" aria-hidden="true"></i>
+          </button>
+          </div>
+          <p>{this.props.place.description}</p>
         </div>
-        <ReactModal isOpen={this.state.showModal} contentLabel="Example Modal" style={customModalStyle} >
-          <h1>{this.props.place.name}</h1>
-          <button onClick={this.handleCloseModal}>Close Modal</button>
+        <ReactModal isOpen={this.state.showModal} contentLabel="{this.props.place.name}" style={customModalStyle}>
+          <div className="modal__title">
+            <h1>{this.props.place.name}</h1>
+            <button onClick={this.handleCloseModal}>
+              <i className="fa fa-times fa-2x" aria-hidden="true"></i>
+            </button>
+          </div>
+          <hr/>
+          <img src={this.props.place.image} />
+          <p>{this.props.place.description}</p>
         </ReactModal>
       </div>
     );
